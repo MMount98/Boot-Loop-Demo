4c2c6273c71d02c33b25abe3b1e83523c83c0c98
Michael Mount <mount.photos@gmail.com>
2023-05-25 13:06:51 -0600

    Changed html to use daisy ui

--- boot-loop-demo/tailwind.config.js
+++ boot-loop-demo/tailwind.config.js
@@ -1,9 +1,44 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        'Oswald': ['Oswald', 'sans-serif']
+      },
+      boxShadow: {
+        "3xl": "0 35px 60px -15px rgba(255, 255, 255)",
+      },
+      visibility: ["group-hover"],
+    },
   },
-  plugins: [],
-}
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      "light",
+      "black",
+      "retro",
+      "cyberpunk",
+      "valentine",
+      "garden",
+      {
+        mytheme: {
+          primary: "#228B22",
 
+          secondary: "#C0C0C0",
+
+          accent: "#00BFFF",
+
+          neutral: "#D3D3D3",
+
+          success: "#00FF00",
+
+          warning: "#FFBF00",
+
+          error: "#FF0000",
+          "base-100": "#228b22",
+        },
+      },
+    ],
+  },
+};
