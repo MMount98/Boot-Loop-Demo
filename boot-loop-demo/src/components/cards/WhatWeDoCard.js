c812c934e1f2eff58ba5171664d78b11b78aab04
Michael Mount <mount.photos@gmail.com>
2023-05-25 22:29:17 -0600

    refactored what we do cards for moble respones

--- boot-loop-demo/src/components/cards/WhatWeDoCard.js
+++ boot-loop-demo/src/components/cards/WhatWeDoCard.js
@@ -6,12 +6,22 @@ export default function WhatWeDoCard(props) {
   return (
     <>
       {content.map((card) => (
-        <div key={card.id} className="card w-72 bg-base-100 shadow-xl">
-          <div className="card-body">
-            <h2 className="card-title font-Oswald text-white">{card.title}</h2>
-            <p>{card.description}</p>
-            <div className="card-actions justify-end">
-              <label htmlFor={`my-modal-${card.id}`} className="btn">
+        <div
+          key={card.id}
+          className="card w-full sm:w-72 sm:m-3 md:m-3 my-4 bg-base-100 shadow-xl rounded-lg overflow-hidden transform transition-all hover:scale-105"
+        >
+          <div className="p-4">
+            <h2 className="text-xl sm:text-2xl card-title font-Oswald text-gray-900 mb-2">
+              {card.title}
+            </h2>
+            <p className="text-sm sm:text-base text-gray-700">
+              {card.description}
+            </p>
+            <div className="card-actions justify-end mt-4">
+              <label
+                htmlFor={`my-modal-${card.id}`}
+                className="btn text-white bg-blue-600 hover:bg-blue-700 rounded transition-colors duration-300"
+              >
                 Learn More
               </label>
             </div>
@@ -22,17 +32,17 @@ export default function WhatWeDoCard(props) {
             className="modal-toggle"
           />
           <div className="modal">
-            <div className="modal-box relative">
+            <div className="modal-box relative p-4 rounded-lg bg-white">
               <label
                 htmlFor={`my-modal-${card.id}`}
-                className="btn btn-sm btn-circle absolute right-2 top-2"
+                className="btn btn-sm btn-circle absolute right-2 top-2 text-gray-700"
               >
                 ✕
               </label>
-              <h3 className="text-3xl font-bold font-Oswald text-white">
+              <h3 className="text-2xl sm:text-3xl font-bold font-Oswald text-gray-900 mb-4">
                 {card.title}
               </h3>
-              {card.details}
+              <p className="text-gray-700">{card.details}</p>
             </div>
           </div>
         </div>
--- boot-loop-demo/src/components/pages/Home.js
+++ boot-loop-demo/src/components/pages/Home.js
@@ -4,13 +4,13 @@ import WhatWeDo from "../content/WhatWeDo";
 export default function Home() {
   return (
     <>
-      <div className="bg-white">
+      <div className="bg-gradient-to-r from-gray-500  to-secondary">
         <div className="flex flex-col md:flex-row items-start justify-around p-4 md:px-48  ">
           <About />
         </div>
         <div className="divider divider-vertical before:bg-base-100 after:bg-base-100 mx-20"></div>
         <h2 className="text-center text-6xl m-6">What We Do</h2>
-        <div className="flex justify-around px-44 py-4">
+        <div className="flex flex-col sm:flex-row justify-around px-4 sm:px-8 md:px-44 py-4">
           <WhatWeDo />
         </div>
       </div>
